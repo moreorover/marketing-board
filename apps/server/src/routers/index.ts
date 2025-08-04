02126c278db5f5266773342990076b3c8f27440c
moreorover <mselvenis@gmail.com>
2025-08-04 19:19:05 +0100

    remove

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -3,16 +3,16 @@ import { protectedProcedure, publicProcedure, router } from "../lib/trpc";
 import { todoRouter } from "./todo";
 
 export const appRouter = router({
-  healthCheck: publicProcedure.query(() => {
-    return "OK";
-  }),
-  privateData: protectedProcedure.query(({ ctx }) => {
-    return {
-      message: `My IP address is ${ctx.ip}`,
-      user: ctx.session.user,
-    };
-  }),
-  todo: todoRouter,
-  listing: listingRouter,
+	healthCheck: publicProcedure.query(() => {
+		return "OK";
+	}),
+	privateData: protectedProcedure.query(({ ctx }) => {
+		return {
+			message: `My IP address is ${ctx.ip}`,
+			user: ctx.session.user,
+		};
+	}),
+	todo: todoRouter,
+	listing: listingRouter,
 });
 export type AppRouter = typeof appRouter;
