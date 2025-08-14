892a487467a3078668d1d39150aad407e0c8fb94
Martin <mselvenis@gmail.com>
2025-08-14 22:16:41 +0100

    cloudflare

--- apps/server/src/lib/context.ts
+++ apps/server/src/lib/context.ts
@@ -1,24 +1,24 @@
 import type { Context as HonoContext } from "hono";
 import { auth } from "./auth";
-import { getConnInfo } from '@hono/node-server/conninfo'
 
 export type CreateContextOptions = {
-  context: HonoContext;
+	context: HonoContext;
 };
 
 export async function createContext({ context }: CreateContextOptions) {
-  const session = await auth.api.getSession({
-    headers: context.req.raw.headers,
-  });
+	const session = await auth.api.getSession({
+		headers: context.req.raw.headers,
+	});
 
-  const ip = context.req.header("x-forwarded-for") || 
-            context.req.header("x-real-ip");
-  
-  return {
-    session,
-    ip,
-  };
+	const ip =
+		context.req.header("CF-Connecting-IP") ||
+		context.req.header("x-forwarded-for") ||
+		context.req.header("x-real-ip");
+
+	return {
+		session,
+		ip,
+	};
 }
-
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
